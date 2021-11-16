e3bd77cae3c914f8be4b2c69cb005db0424c4211
rwozniak89 <rwozniak89@gmail.com>
2021-11-16 14:53:50 +0100

    StartNewWorkflowAction cz1

---
+++ WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomActions/StartNewWorkflowActionConfig.cs
@@ -0,0 +1,17 @@
+﻿using WebCon.WorkFlow.SDK.Common;
+using WebCon.WorkFlow.SDK.ConfigAttributes;
+
+namespace WebCon.BpsExt.Training.CustomActions
+{
+    public class StartNewWorkflowActionConfig : PluginConfiguration
+    {
+        [ConfigEditableText(DisplayName = "Form type ID", IsRequired = true)]
+        public int DoctypeID { get; set; }
+
+        [ConfigEditableText(DisplayName = "Workflow ID", IsRequired = true)]
+        public int WorkflowID { get; set; }
+
+        [ConfigEditableText(DisplayName = "Starting path ID", IsRequired = true)]
+        public int StartingPathID { get; set; }
+    }
+}
--- WebCon.BpsExt.Training/WebCon.BpsExt.Training/Properties/AssemblyInfo.cs
+++ WebCon.BpsExt.Training/WebCon.BpsExt.Training/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.7")]
-[assembly: AssemblyFileVersion("1.0.0.7")]
+[assembly: AssemblyVersion("1.0.0.8")]
+[assembly: AssemblyFileVersion("1.0.0.8")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
