7586a3a8d799cadf9d752680dfc0c27499411706
rwozniak89 <rwozniak89@gmail.com>
2021-11-17 15:44:02 +0100

    ttt

--- WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomDataSources/RequestItemsDataSource.cs
+++ WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomDataSources/RequestItemsDataSource.cs
@@ -12,6 +12,7 @@ namespace WebCon.BpsExt.Training.CustomDataSources
 {
     public class RequestItemsDataSource : CustomDataSource<RequestItemsDataSourceConfig>
     {
+        private const string CaseSensitiveFunction = "CaseSensitive";
         public override DataTable GetData(SearchConditions searchConditions)
         {
             var result = GenerateDataTable();
@@ -22,10 +23,55 @@ namespace WebCon.BpsExt.Training.CustomDataSources
             foreach (DataRow item in items.Rows)
             {
                 var newRow = FillNewRow(result.NewRow(), item, categoryPersons);
+                if(CheckConditions(newRow, searchConditions))
                 result.Rows.Add(newRow);
             }
 
             return result;
+        }
+
+        private bool CheckConditions(DataRow newRow, SearchConditions searchConditions)
+        {
+            if (searchConditions == null)
+                return true;
+
+            foreach(var condition in searchConditions.AndConditions)
+            {
+                if(!CheckCondition(newRow, condition))
+                {
+                    return false;
+                }
+            }
+
+            if(!searchConditions.OrConditions.Any())
+            {
+                return true;
+            }
+
+            foreach (var condition in searchConditions.OrConditions)
+            {
+                if (CheckCondition(newRow, condition))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool CheckCondition(DataRow newRow, SearchCondition condition)
+        {
+
+            var comparison = StringComparison.CurrentCultureIgnoreCase;
+            if (!string.IsNullOrEmpty(condition.ColumnWrapingFunction) && condition.ColumnWrapingFunction.Equals(CaseSensitiveFunction))
+                comparison = StringComparison.CurrentCulture;
+
+            if (condition.Match == MatchType.StartsWith)
+            {
+                return newRow[condition.ColumnName].ToString().StartsWith(condition.Value, comparison);
+            }
+            else
+                return newRow[condition.ColumnName].ToString().Equals(condition.Value, comparison);
         }
 
         private DataTable GenerateDataTable()
@@ -69,9 +115,13 @@ namespace WebCon.BpsExt.Training.CustomDataSources
 
         public override List<DataSourceColumn> GetColumns()
         {
+
+            var supportedOperators = new[] { MatchType.Equals }.ToList();
+            var supportedFunction = new[] { new WrappingFunction(CaseSensitiveFunction, "CaseSensitive()") }.ToList();
+
             return new List<DataSourceColumn>()
             {
-                new DataSourceColumn("Category","Category"),
+                new DataSourceColumn("Category","Category") { SupportedOperators = supportedOperators, SupportedFunctions = supportedFunction},
                 new DataSourceColumn("Item","Item"),
                 new DataSourceColumn("ID","ID"),
                 new DataSourceColumn("Person","Person")
