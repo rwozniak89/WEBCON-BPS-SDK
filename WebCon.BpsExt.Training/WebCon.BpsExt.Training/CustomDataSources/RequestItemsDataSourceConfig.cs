fe1a83b13a16ada50ca7e0d1b17934dc31d59981
rwozniak89 <rwozniak89@gmail.com>
2021-11-17 12:15:42 +0100

    RequestItemsDataSource

---
+++ WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomDataSources/RequestItemsDataSourceConfig.cs
@@ -0,0 +1,20 @@
+﻿using WebCon.WorkFlow.SDK.Common;
+using WebCon.WorkFlow.SDK.ConfigAttributes;
+
+namespace WebCon.BpsExt.Training.CustomDataSources
+{
+    public class RequestItemsDataSourceConfig : PluginConfiguration
+    {
+        [ConfigEditableDataSourceID(DisplayName = "Responsible persons", Description = "Datasource of persons responsible for resource categories", IsRequired = true)]
+        public int PersonsDataSourceID { get; set; }
+
+        [ConfigEditableText(DisplayName = "Column of the person", Description = "Name of the column with the person responsible for resource categories", IsRequired = true)]
+        public string PersonColumnName { get; set; }
+
+        [ConfigEditableText(DisplayName = "Column of the category", Description = "Name of the column with the resource category", IsRequired = true)]
+        public string CategoryColumnName { get; set; }
+
+        [ConfigEditableDataSourceID(DisplayName = "Resources", SourcesType = DataSourceType.WebServiceREST, Description = "Resources datasource", IsRequired = true)]
+        public int ResourcesDataSourceID { get; set; }
+    }
+}
