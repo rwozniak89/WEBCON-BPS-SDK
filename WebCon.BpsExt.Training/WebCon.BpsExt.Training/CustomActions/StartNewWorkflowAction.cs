e1bf54e8891e21b2cd7a94bec5db45ae505485b3
rwozniak89 <rwozniak89@gmail.com>
2021-11-17 10:35:28 +0100

    start new workflow and transaction

--- WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomActions/StartNewWorkflowAction.cs
+++ WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomActions/StartNewWorkflowAction.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using WebCon.WorkFlow.SDK.ActionPlugins;
 using WebCon.WorkFlow.SDK.ActionPlugins.Model;
 using WebCon.WorkFlow.SDK.Documents;
 using WebCon.WorkFlow.SDK.Documents.Model;
+using WebCon.WorkFlow.SDK.Documents.Model.Attachments;
+using WebCon.WorkFlow.SDK.Tools.Data;
 
 namespace WebCon.BpsExt.Training.CustomActions
 {
@@ -10,17 +13,66 @@ namespace WebCon.BpsExt.Training.CustomActions
     {
         public override void Run(RunCustomActionParams args)
         {
+            var elementId = GenetrateNewDocument(args.Context.CurrentDocument);
+            var url = $"{Configuration.PortalUrl}/db/1007/app/6/tasks/element/{elementId}/form";
+
+            //if (args.TransitionInfo != null)
+            //    args.TransitionInfo.RedirectUrl(url);
+            //else
+            //    base.CustomJavascript = $"alert(\"Test\"); window.open('{url}')";
+
+        }
+
+
+        private int GenetrateNewDocument(ExistingDocumentData parentDocument)
+        {
             var newDocument = DocumentsManager.GetNewDocument(new GetNewDocumentParams(Configuration.DoctypeID, Configuration.WorkflowID)
-            {
-                SkipPermissionsCheck = true
-            });
+            { SkipPermissionsCheck = true , ParentDocumentID = parentDocument.ID});
 
             //newDocument.SetFieldValue(Configuration.FieldId, "")
 
-            DocumentsManager.StartNewWorkFlow(new StartNewWorkFlowParams(newDocument, Configuration.StartingPathID)
+            foreach (var filed in Configuration.FieldValues)
+                newDocument.SetFieldValue(filed.FieldId, filed.Value);
+
+            //if(parentDocument.Attachments.Any())
+            //{
+            //    var att = parentDocument.Attachments.First();
+            //    //new NewAttachmentData() { }
+            //    newDocument.Attachments.AddNew(att);
+            //}
+
+
+            var result = DocumentsManager.StartNewWorkFlow(new StartNewWorkFlowParams(newDocument, Configuration.StartingPathID)
+            { SkipPermissionsCheck = true });
+
+            parentDocument.SetFieldValue(Configuration.SygnatureFieldId, result.CreatedDocumentID);
+
+            return result.CreatedDocumentID;
+        }
+
+        public override void RunWithoutDocumentContext(RunCustomActionWithoutContextParams args)
+        {
+            using (var transactionScope = new BPSTransactionScope(args.Context))
             {
-                SkipPermissionsCheck = true
-            });
+                var parentDocumen = GetParent();
+
+                //DocumentAttachmentsManager.GetAttachments(new GetAttachmentsParams() { DocumentId = parentDocumen.ID });
+
+                GenetrateNewDocument(parentDocumen);
+
+                DocumentsManager.UpdateDocument(new UpdateDocumentParams(parentDocumen) { ForceCheckout = true, SkipPermissionsCheck = true });
+
+                transactionScope.Complete();
+            }
+
+
+               
+        }
+
+        private ExistingDocumentData GetParent()
+        {
+            var parentId = (int)SqlExecutionHelper.ExecSqlCommandScalarOutsideTransaction(Configuration.ParentIdQuery);
+            return DocumentsManager.GetDocumentByID(parentId, true);
         }
     }
 }
--- WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomActions/StartNewWorkflowActionConfig.cs
+++ WebCon.BpsExt.Training/WebCon.BpsExt.Training/CustomActions/StartNewWorkflowActionConfig.cs
@@ -1,4 +1,5 @@
-﻿using WebCon.WorkFlow.SDK.Common;
+﻿using System.Collections.Generic;
+using WebCon.WorkFlow.SDK.Common;
 using WebCon.WorkFlow.SDK.ConfigAttributes;
 
 namespace WebCon.BpsExt.Training.CustomActions
@@ -13,5 +14,30 @@ namespace WebCon.BpsExt.Training.CustomActions
 
         [ConfigEditableText(DisplayName = "Starting path ID", IsRequired = true)]
         public int StartingPathID { get; set; }
+
+        //https://srv38.webconbps.com/db/1007/app/6/tasks/element/12/form
+
+        [ConfigEditableText("PortalUrl", isRequired: true, DefaultText = ("https://srv38.webconbps.com/"))]
+        public string PortalUrl { get; set; }
+
+        [ConfigEditableFormFieldID("Created element id sygnature")]
+        public int SygnatureFieldId { get; set; }
+
+        [ConfigEditableGrid(DisplayName = "Fields valuess")]
+        public List<FieldValue> FieldValues { get; set; }
+
+        [ConfigEditableText("ParentId", IsRequired = true, Multiline = true, TagEvaluationMode = EvaluationMode.SQL)] //, DefaultText = "{WFD_ID}")]
+        public string ParentIdQuery { get; set; }
+    }
+
+
+    public class FieldValue
+    {
+        [ConfigEditableGridColumn(DisplayName = "Value")]
+        public string Value { get; set; }
+
+        [ConfigEditableGridColumnFormFieldID(DisplayName = "Field ID")]
+        public int FieldId { get; set; }
+
     }
 }
